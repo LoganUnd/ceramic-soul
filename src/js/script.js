eb900c35c6b96ee39adc61615aa8af22b6f14368
Sergey <sergey.kyshnur@gmail.com>
2025-05-26 19:59:03 +0200

    create new page

--- src/js/script.js
+++ src/js/script.js
@@ -10,6 +10,20 @@ import "purecss/build/grids-responsive-min.css"
 
 import "/src/sass/style.scss";
 
+const burger = document.querySelector(".burger"),
+	close = document.querySelector(".header__menu-close"),
+	menu = document.querySelector(".header__menu");
+
+burger.addEventListener("click", () => {
+	menu.classList.add("header__menu-active");
+	document.body.style.overflow = "hidden";
+});
+
+close.addEventListener("click", () => {
+	menu.classList.remove("header__menu-active");
+	document.body.style.overflow = "";
+});
+
 try {
   new Swiper('.works__slider', {
     slidesPerView: 1,
@@ -50,3 +64,25 @@ try {
 console.log("Hello world")
 
 } catch (e) {}
+
+try {
+	const tabs = document.querySelectorAll(".catalog__tab");
+	const contents = document.querySelectorAll(".catalog__content-item");
+
+	tabs.forEach((tab, index) => {
+		tab.addEventListener("click", () => {
+			// Удаляем активный класс у всех табов и контента
+			tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
+			contents.forEach((c) => (c.style.display = "none"));
+
+			// Добавляем активный класс к нажатому табу и показываем соответствующий контент
+			tab.classList.add("catalog__tab_active");
+			contents[index].style.display = "block";
+		});
+	});
+
+	// Показываем первый контент при загрузке
+	contents.forEach((c, i) => (c.style.display = i === 0 ? "block" : "none"));
+} catch (e) {}
+
+// Обратите внимание, что значение block (в двух местах) можно спокойно
