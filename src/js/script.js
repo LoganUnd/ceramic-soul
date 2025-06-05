3e30920828dd1db599dd250294c884e4af9140c9
Sergey <sergey.kyshnur@gmail.com>
2025-06-05 22:11:57 +0200

    New page it full adapted for all screens

--- src/js/script.js
+++ src/js/script.js
@@ -67,22 +67,34 @@ console.log("Hello world")
 
 try {
 	const tabs = document.querySelectorAll(".catalog__tab");
-	const contents = document.querySelectorAll(".catalog__content-item");
+	const contents = document.querySelectorAll(".catalog__content-item, .catalog__content-item-3");
 
 	tabs.forEach((tab, index) => {
 		tab.addEventListener("click", () => {
-			// Удаляем активный класс у всех табов и контента
+			// Remove active class from all tabs
 			tabs.forEach((t) => t.classList.remove("catalog__tab_active"));
-			contents.forEach((c) => (c.style.display = "none"));
+			
+			// Hide all content sections
+			contents.forEach((c) => {
+				c.style.display = "none";
+			});
 
-			// Добавляем активный класс к нажатому табу и показываем соответствующий контент
+			// Add active class to clicked tab
 			tab.classList.add("catalog__tab_active");
-			contents[index].style.display = "block";
+			
+			// Show corresponding content
+			if (contents[index]) {
+				contents[index].style.display = "flex";
+			}
 		});
 	});
 
-	// Показываем первый контент при загрузке
-	contents.forEach((c, i) => (c.style.display = i === 0 ? "block" : "none"));
-} catch (e) {}
+	// Show first content on page load
+	contents.forEach((c, i) => {
+		c.style.display = i === 0 ? "flex" : "none";
+	});
+} catch (e) {
+	console.error("Error in tab switching:", e);
+}
 
 // Обратите внимание, что значение block (в двух местах) можно спокойно
